83bbb4a8a1a083f4e110a1e71dddf0edc7315784
Report <tutuiga@gmail.com>
2025-07-05 20:20:02 -0300

    Add IUser interface for user data abstraction
    Apresenta a classe base abstrata IUser, que define propriedades e setters para campos relacionados ao usuário, como ID, nome, e-mail, telefone e contato. Essa interface estabelece um contrato para o tratamento de dados do usuário e impõe a implementação em subclasses.

---
+++ interfaces/IUser.py
@@ -0,0 +1,80 @@
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from models.contact import Contact
+
+
+class IUser(ABC):
+    ########################################################
+    @property
+    def id_user(self) -> int:
+        """Get the user ID."""
+        pass
+
+    @id_user.setter
+    @abstractmethod
+    def id_user(self, value: int):
+        """Set the user ID."""
+        pass
+
+    #######################################################
+    @property
+    def first_name(self) -> str:
+        """Get the user's first name."""
+        pass
+
+    @first_name.setter
+    @abstractmethod
+    def first_name(self, value: str):
+        """Set the user's first name."""
+        pass
+
+    #######################################################
+    @property
+    def last_name(self) -> str:
+        """Get the user's last name."""
+        pass
+
+    @last_name.setter
+    @abstractmethod
+    def last_name(self, value: str):
+        """Set the user's last name."""
+        pass
+
+    #######################################################
+    @property
+    def email(self) -> str:
+        """Get the user's email."""
+        pass
+
+    @email.setter
+    @abstractmethod
+    def email(self, value: str):
+        """Set the user's email."""
+        pass
+
+    #######################################################
+    @property
+    def phone(self) -> str:
+        """Get the user's phone number."""
+        pass
+
+    @phone.setter
+    @abstractmethod
+    def phone(self, value: str):
+        """Set the user's phone number."""
+        pass
+
+    #######################################################
+    @property
+    def contact(self) -> 'Contact':
+        """Get the user's contact information."""
+        pass
+
+    @contact.setter
+    @abstractmethod
+    def contact(self, value: 'Contact'):
+        """Set the user's contact information."""
+        pass
+
