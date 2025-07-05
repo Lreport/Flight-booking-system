8025ced5fbf2bd6ff5cd4200f63724ed13c7dca3
Report <tutuiga@gmail.com>
2025-07-05 20:49:22 -0300

    Add Crew model extending User with role attribute
    Apresenta uma nova classe Crew que herda de User e adiciona uma propriedade role com getter e setter. Isso permite representar membros da tripulação com funções específicas no sistema.

---
+++ models/crew.py
@@ -0,0 +1,19 @@
+from models.user import User
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from models.contact import Contact
+
+
+class Crew(User):
+    def __init__(self, id_user:int, first_name:str, last_name:str, contact: 'Contact', role:str):
+        super().__init__(id_user, first_name, last_name, contact)
+        self._role = role
+
+    @property
+    def role(self):
+        return self._role
+
+    @role.setter
+    def role(self, value):
+        self._role = value
