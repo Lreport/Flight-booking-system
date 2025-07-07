5b899c69f6fdd9be4ff1b90e89cb2ba5e2b526ea
Report <tutuiga@gmail.com>
2025-07-07 01:56:50 -0300

    Fix interface passenger

--- interfaces/Ipassenger.py
+++ interfaces/Ipassenger.py
@@ -7,52 +7,48 @@ if TYPE_CHECKING:
 class IPassenger(ABC):
     ########################################################
     @property
-    @abstractmethod
     def id_user(self) -> int:
-        """Get the user ID."""
+        """Get the passenger ID."""
         pass
 
     @id_user.setter
     @abstractmethod
     def id_user(self, value: int):
-        """Set the user ID."""
+        """Set the passenger ID."""
         pass
 
-    ########################################################
+    #######################################################
     @property
-    @abstractmethod
     def first_name(self) -> str:
-        """Get the user's first name."""
+        """Get the passenger's first name."""
         pass
 
     @first_name.setter
     @abstractmethod
     def first_name(self, value: str):
-        """Set the user's first name."""
+        """Set the passenger's first name."""
         pass
 
-    ########################################################
+    #######################################################
     @property
-    @abstractmethod
     def last_name(self) -> str:
-        """Get the user's last name."""
+        """Get the passenger's last name."""
         pass
 
     @last_name.setter
     @abstractmethod
     def last_name(self, value: str):
-        """Set the user's last name."""
+        """Set the passenger's last name."""
         pass
 
-    ########################################################
+    #######################################################
     @property
-    @abstractmethod
     def contact(self) -> 'Contact':
-        """Get the contact information of the passenger."""
+        """Get the passenger's contact information."""
         pass
 
     @contact.setter
     @abstractmethod
     def contact(self, value: 'Contact'):
-        """Set the contact information of the passenger."""
+        """Set the passenger's contact information."""
         pass
