544cf53187f44931f4dddc7284d7b2fa930879fc
Report <tutuiga@gmail.com>
2025-07-07 09:25:59 -0300

    Update Passenger.py

--- models/Passenger.py
+++ models/Passenger.py
@@ -1,49 +1,13 @@
 from typing import TYPE_CHECKING
-from interfaces.Ipassenger import IPassenger
+from models.user import User
 
 if TYPE_CHECKING:
     from models.contact import Contact
-    from models.user import User
 
-class Passenger(IPassenger):
-    def __init__(self, id_user: int, first_name: str, last_name: str, contact: 'Contact' = None):
-        self._id_user       = id_user
-        self._first_name    = first_name
-        self._last_name     = last_name
-        self._contact       = contact
+class Passenger(User):
+    def __init__(self, id_user: int, name: str, contact=None):
+        super().__init__(id_user, name, contact)
         self._assigned_seat = None
-
-    @property
-    def id_user(self):
-        return self._id_user
-
-    @id_user.setter
-    def id_user(self, value):
-        self._id_user = value
-
-    @property
-    def first_name(self):
-        return self._first_name
-
-    @first_name.setter
-    def first_name(self, value):
-        self._first_name = value
-
-    @property
-    def last_name(self):
-        return self._last_name
-
-    @last_name.setter
-    def last_name(self, value):
-        self._last_name = value
-
-    @property
-    def contact(self):
-        return self._contact
-
-    @contact.setter
-    def contact(self, value):
-        self._contact = value
 
     @property
     def assigned_seat(self):
@@ -65,4 +29,4 @@ class Passenger(IPassenger):
         self._assigned_seat = flight.seats[seat_id - 1]
 
     def __str__(self):
-        return f"Passageiro: {self.first_name} {self.last_name} (ID: {self.id_user})"
+        return f"Passageiro: {self.name} (ID: {self.id_user})"
