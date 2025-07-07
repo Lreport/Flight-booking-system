a84edd099446deb941bacf4ee45eca7dda8928f5
Report <tutuiga@gmail.com>
2025-07-07 09:25:41 -0300

    Update User.py

--- models/User.py
+++ models/User.py
@@ -6,11 +6,10 @@ if TYPE_CHECKING:
     from models.contact import Contact
 
 class User(Iuser):
-    def __init__(self, id_user: int, first_name: str, last_name: str, contact: 'Contact'):
-        self._id_user           = id_user
-        self._first_name        = first_name
-        self._last_name         = last_name
-        self._contact           = contact
+    def __init__(self, id_user: int, name: str, contact=None):
+        self._id_user = id_user
+        self._name = name
+        self._contact = contact
 
     @property
     def id_user(self):
@@ -21,20 +20,12 @@ class User(Iuser):
         self._id_user = value
 
     @property
-    def first_name(self):
-        return self._first_name
+    def name(self):
+        return self._name
 
-    @first_name.setter
-    def first_name(self, value):
-        self._first_name = value
-
-    @property
-    def last_name(self):
-        return self._last_name
-
-    @last_name.setter
-    def last_name(self, value):
-        self._last_name = value
+    @name.setter
+    def name(self, value):
+        self._name = value
 
     @property
     def contact(self):
