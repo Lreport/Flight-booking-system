9aeb1a42bd552e4cd6149225cb963208985cc340
Report <tutuiga@gmail.com>
2025-07-07 09:26:08 -0300

    Update crew.py

--- models/crew.py
+++ models/crew.py
@@ -1,49 +1,14 @@
-from interfaces.Icrew import Icrew
 from typing import TYPE_CHECKING
+from models.user import User
 
 if TYPE_CHECKING:
     from models.contact import Contact
 
 
-class Crew(Icrew):
-    def __init__(self, id_user:int, first_name:str, last_name:str, contact: 'Contact' = None, role:str = "Crew"):
-        self._id_user       = id_user
-        self._first_name    = first_name
-        self._last_name     = last_name
-        self._contact       = contact
-        self._role          = role
-
-    @property
-    def id_user(self):
-        return self._id_user
-
-    @id_user.setter
-    def id_user(self, value):
-        self._id_user = value
-
-    @property
-    def first_name(self):
-        return self._first_name
-
-    @first_name.setter
-    def first_name(self, value):
-        self._first_name = value
-
-    @property
-    def last_name(self):
-        return self._last_name
-
-    @last_name.setter
-    def last_name(self, value):
-        self._last_name = value
-
-    @property
-    def contact(self):
-        return self._contact
-
-    @contact.setter
-    def contact(self, value):
-        self._contact = value
+class Crew(User):
+    def __init__(self, id_user: int, name: str, contact=None, role: str = "Crew"):
+        super().__init__(id_user, name, contact)
+        self._role = role
 
     @property
     def role(self):
@@ -54,19 +19,19 @@ class Crew(Icrew):
         self._role = value
 
     def __str__(self):
-        return f"{self._role}: {self.first_name} {self.last_name} (ID: {self.id_user})"
+        return f"{self._role}: {self.name} (ID: {self.id_user})"
 
 
 class Pilot(Crew):
-    def __init__(self, id_user:int, first_name:str, last_name:str, contact: 'Contact' = None, role:str = 'Pilot'):
-        super().__init__(id_user, first_name, last_name, contact, role)
+    def __init__(self, id_user: int, name: str, contact=None, role: str = 'Pilot'):
+        super().__init__(id_user, name, contact, role)
 
 
 class Copilot(Crew):
-    def __init__(self, id_user:int, first_name:str, last_name:str, contact: 'Contact' = None, role:str = 'Copilot'):
-        super().__init__(id_user, first_name, last_name, contact, role)
+    def __init__(self, id_user: int, name: str, contact=None, role: str = 'Copilot'):
+        super().__init__(id_user, name, contact, role)
 
 
 class Commissar(Crew):
-    def __init__(self, id_user:int, first_name:str, last_name:str, contact: 'Contact' = None, role:str = 'Commissar'):
-        super().__init__(id_user, first_name, last_name, contact, role)
+    def __init__(self, id_user: int, name: str, contact=None, role: str = 'Commissar'):
+        super().__init__(id_user, name, contact, role)
